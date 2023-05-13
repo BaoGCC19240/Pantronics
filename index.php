8e2383b502549df37630be7d979f8a3a7237e3fc
BaoGCC19240 <91667147+BaoGCC19240@users.noreply.github.com>
2023-05-13 19:19:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -99,6 +99,38 @@ while ($row = mysqli_fetch_assoc($res)) {
         </form>
       </div>
     </div>
+
+<!-- Messenger Plugin chat Code -->
+<div id="fb-root"></div>
+
+<!-- Your Plugin chat code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "113772581716218");
+  chatbox.setAttribute("attribution", "biz_inbox");
+</script>
+
+<!-- Your SDK code -->
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v16.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
+
     <script>
       var productLink = document.getElementById("product-link");
       var productList = document.getElementById("product-list");
